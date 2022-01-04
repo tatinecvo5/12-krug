c844ef7b15b5330b55139838457eb33eeec08195
User <User@SERGEY>
2022-01-04 20:14:01 +0300

    Добавьте файлы проекта.

---
+++ 12 krug/Krug.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_krug
+{
+    static class Krug
+    {
+        static public string GetLength(int R)
+        {
+                        //return 2 * R * Math.PI;
+            double L= 2 * R * Math.PI;
+            return "длина окружности S={L}";
+        }
+        static public double GetSquare(int R)
+        {
+            return R * R * Math.PI;
+
+        }
+        static public string GetPlace(int R, int X, int Y)
+        {
+            double G = Math.Sqrt(X * X + Y * Y);
+            if (G < R)
+                return "Точка с координатами X, Y в круге";
+            return "Точка с координатами X, Y за кругом";
+        }
+    }
+}
---
+++ 12 krug/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_krug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите радиус круга R=");
+            int R = Convert.ToInt32(Console.ReadLine());
+            //double L = 2 * R * Math.PI;
+            /*double S = R * R * Math.PI;
+                        Console.WriteLine("длина окружности L={0:f3}", 2 * R * Math.PI);
+            Console.WriteLine("длина окружности S={0:f3}", S);*/
+            Console.Write("введите координату X=");
+            int X = Convert.ToInt32(Console.ReadLine());
+            Console.Write("введите координату Y=");
+            int Y = Convert.ToInt32(Console.ReadLine());
+            /*double G = Math.Sqrt(X * X + Y * Y);
+            if (G < R)
+            {
+                Console.Write("Точка с координатами X, Y в круге");
+            }
+            else
+                if (G > R)
+            {
+                Console.Write("Точка с координатами X, Y за кругом");
+            }
+            else
+            {
+                Console.Write("Точка с координатами X, Y на окружности");
+            }
+            Console.ReadKey();*/
+            string length = Krug.GetLength(R);
+            double square = Krug.GetSquare(R);
+            string place = Krug.GetPlace(R,X,Y);
+                        Console.WriteLine($"{length:f3} \n{square:f3} \n{place}");
+            Console.ReadKey();
+        }
+    }
+}
